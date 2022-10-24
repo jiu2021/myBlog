3a9a27b764bea9d35d59ae360d3397503f53f3fe
Kai Jian <1358940365@qq.com>
2022-10-24 12:35:12 +0800

    about页脚

--- fe/src/pages/About/index.vue
+++ fe/src/pages/About/index.vue
@@ -17,6 +17,7 @@
         </div>
       </div>
     </transition>
+    <Footer />
   </div>
 </template>
 
--- fe/vue.config.js
+++ fe/vue.config.js
@@ -8,11 +8,11 @@ module.exports = {
   //关闭eslint
   lintOnSave: false,
   //代理跨域
-  devServer: {
+  /*devServer: {
     proxy: {
       '/': {
         target: 'http://jiu99.online:8889/api',
       }
     }
-  }
+  }*/
 }
